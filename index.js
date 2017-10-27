1620bdd993556f13e40568b6d58c045d7a7d9c98
Ludvig <ludvig.ohlsson@gmail.com>
2017-10-27 23:59:18 +0200

    qqq

--- index.js
+++ index.js
@@ -8,7 +8,11 @@ var path = require('path');
 
 app.use(express.static(__dirname));
 
-app.listen('5000');
+app.set('port', (process.env.PORT || 5000));
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
 
 console.log('Listening on port 8081');
 
